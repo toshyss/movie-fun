128e44b403e17a9793ba196e8d24797669607a13
toshyss <me@exmample.com>
2019-10-24 11:37:06 +0900

    spring bootfication

---
+++ src/main/java/org/superbiz/moviefun/Application.java
@@ -0,0 +1,23 @@
+package org.superbiz.moviefun;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.context.annotation.Bean;
+
+import javax.servlet.ServletRegistration;
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+    @Bean
+    public ServletRegistrationBean servletRegistration(ActionServlet actionservlet){
+
+        ServletRegistrationBean servletRegistrationBean = new ServletRegistrationBean(actionservlet, "/moviefun/*");
+        return servletRegistrationBean;
+    }
+}
